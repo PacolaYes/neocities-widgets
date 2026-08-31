c78b6549a5b9568f501ac82f2e181a65c017faec
Pacola <149804664+PacolaYes@users.noreply.github.com>
2026-08-31 20:36:34 -0300

    add english wikipedia api query thingy

---
+++ src/js/wikipedia.js
@@ -0,0 +1,19 @@
+
+async function getWikipediaInfo(params) {
+    const urlParams = new URLSearchParams(params)
+    try {
+	const response = await fetch(`https://en.wikipedia.org/w/api.php?format=json&${params}`);
+	if (!response.ok) {
+	    throw new Error(`Response status: ${response.status}`);
+	}
+
+	const info = await response.json();
+	window.parent.postMessage(info, "*");
+    } catch (error) {
+	console.error(error.message);
+    }
+}
+
+document.addEventListener("message", (event) => {
+    getWikipediaInfo(event.data);
+})
