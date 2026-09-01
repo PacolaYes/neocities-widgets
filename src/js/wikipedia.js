6eb69e04fc2b8e2cb960ce85847d43ec395100d9
Pacola <149804664+PacolaYes@users.noreply.github.com>
2026-08-31 21:24:10 -0300

    i love debug prints

--- src/js/wikipedia.js
+++ src/js/wikipedia.js
@@ -1,5 +1,7 @@
 
 async function getWikipediaInfo(params) {
+    console.log(params);
+
     const urlParams = new URLSearchParams(params)
     try {
 	const response = await fetch(`https://en.wikipedia.org/w/api.php?format=json&${params}`);
